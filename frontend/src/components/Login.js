1cec2f105db8ade0bc475034ac107d84075253c8
Akseli Hyvönen <topiakseli@outlook.com>
2024-04-12 02:56:03 +0300

    Fixed login screen

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -6,18 +6,20 @@ function Login() {
         <>
             <main>
                 <div className='login-screen'>
-                    <div className='login-header'>
-                        <div className='login-title'>ProjectPilot</div>
-                        <div className='login-title-undertext'>Project handling made easy</div>
+                    <div>
+                        <div className='login-header'>
+                            <div className='login-title'>ProjectPilot</div>
+                            <div className='login-title-undertext'>Project handling made easy</div>
+                        </div>
+                        <form className='login-form'>
+                            <div className='login-text'>username:</div>
+                            <input className='login-input'/>
+                            <div className='login-text'>password: <a className='login-recover-password'>Forgot password?</a></div>
+                            <input className='login-input'/>
+                            <div className='login-text'>Don't Have an account? <a className='login-register'>Create one</a></div>
+                            <button className='login-button'>LOG IN</button>
+                        </form>
                     </div>
-                    <form className='login-form'>
-                        <div className='login-text'>username:</div>
-                        <input className='login-input'/>
-                        <div className='login-text'>password: <a className='login-recover-password'>Forgot password?</a></div>
-                        <input className='login-input'/>
-                        <div className='login-text'>Don't Have an account? <a className='login-register'>Create one</a></div>
-                        <button className='login-button'>LOG IN</button>
-                    </form>
 
                 </div>
                 <div className='image'>
